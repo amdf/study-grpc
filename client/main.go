04043db574b0495a00aacc19dacaa6d55a7dc4bf
amdf <787535+amdf@users.noreply.github.com>
2022-01-19 15:12:33 +0300

    implemented another call

--- client/main.go
+++ client/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math/rand"
 	"time"
 
 	pb "github.com/amdf/study-grpc/svc"
@@ -22,10 +24,35 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
+	//simple method:
+
 	r, err := c.SimpleFunction(ctx, &pb.SimpleQuery{Text: "SoMeTeXt"})
 	if err != nil {
-		log.Fatalln("fail to call SimpleFunction", err)
+		log.Fatalln("fail to call SimpleFunction()", err)
 	}
 	i := r.GetRuneCount()
-	log.Println("Rune count:", i)
+	log.Println("I. Rune count:", i)
+
+	//method with argument stream:
+	stream, err := c.Sum(context.Background())
+	if err != nil {
+		log.Fatalln("fail to call Sum()", err)
+	}
+	for i := 0; i < rand.Intn(20); i++ {
+		var n pb.Number
+		n.Value = uint32(rand.Int31n(25))
+		err = stream.Send(&n)
+		if err != nil {
+			log.Fatalln("fail to send Sum() argument", err)
+		}
+	}
+	result, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalln("error get Sum() result", err)
+	}
+	fmt.Println("II: Sum result:", result.Value)
+
+	//method with result stream:
+	// ctx2, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	// c.GenerateWords(ctx2, &pb.WantWords{Count: 10, Delay: durationpb.New(time.Microsecond * 100)})
 }
